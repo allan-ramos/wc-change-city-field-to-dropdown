a72697b2f79bbc0fe2cef42fd7c4fc4209571869
allan-ramos <76536015+allan-ramos@users.noreply.github.com>
2020-12-30 21:03:24 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,12 @@
 <?php
 
-
+// Change city text field to dropdown
 function allan_change_city_to_dropdown( $fields ) {
 
 	$city_args = wp_parse_args( array(
 		'type' => 'select',
 		'options' => array(
-			'Seleccione' => 'Seleccione',
-      'Ciudad 1' => 'Ciudad 1',
-      'Ciudad 2' => 'Ciudad 2',
+			'Seleccione' => 'Seleccione'
 		),
 	), $fields['shipping']['shipping_city'] );
 
@@ -20,5 +18,29 @@ function allan_change_city_to_dropdown( $fields ) {
 
 }
 add_filter( 'woocommerce_checkout_fields', 'allan_change_city_to_dropdown' );
+
+
+
+// Add extra custom field to checkout
+function add_extra_field_to_checkout( $fields ) {
+
+	$depto_args = wp_parse_args( array(
+		'type' => 'select',
+		'options' => array(
+			'Seleccione' => 'Seleccione',
+			'Francisco Morazan' => 'Francisco Morazan',
+			'Olancho' => 'Olancho',
+			'Valle' => 'Valle',
+		),
+	), $fields['shipping']['shipping_depto'] );
+
+	$fields['shipping']['shipping_depto'] = $depto_args;
+	$fields['billing']['billing_depto'] = $depto_args; 
+	$fields['billing']['billing_depto']['priority'] = 80;
+
+	return $fields;
+
+}
+add_filter( 'woocommerce_checkout_fields', 'add_extra_field_to_checkout' );
 
 ?>
